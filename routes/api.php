8e0d262513901893d985338c6a7f6a8e0e262634
mpluiz <marcospaulo.luiz@gmail.com>
2020-08-01 21:26:41 -0300

    Adding Route to trending movies

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+$version = 'v1';
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix'=> $version], function () {
+    Route::group(['prefix' => 'trending'], function () {
+        Route::get('{media_type}/{time_window}',   'TrendingController@getTrending');
+    });
 });
