d2f5c0ccedc954dda8dcd9e99eca9650207a4fa9
mpluiz <marcospaulo.luiz@gmail.com>
2020-08-01 21:43:10 -0300

    Adding Route to genres list

--- routes/api.php
+++ routes/api.php
@@ -9,4 +9,7 @@ Route::group(['prefix'=> $version], function () {
     Route::group(['prefix' => 'trending'], function () {
         Route::get('{media_type}/{time_window}',   'TrendingController@getTrending');
     });
+    Route::group(['prefix' => 'genre'], function () {
+        Route::get('movie/list',   'GenresController@getGenresList');
+    });
 });
